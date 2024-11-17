d5c4bf37e4df64235a5e737bb7fe8484beddec20
mohamed.sabbah <mohamed.sabbah@gizasystems.com>
2024-11-17 23:12:53 +0200

    Create Skeleton for notification and user management servies

--- notification-service/src/main/java/com/app/notification/management/entity/NotificationEntity.java
+++ notification-service/src/main/java/com/app/notification/management/entity/NotificationEntity.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "notification")
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class NotificationEntity extends BaseEntity {
     @Id
     private Long id;
