36f765e6b1e4867aa950dbe908c27aa0c1556c15
mohamed.sabbah <mohamed.sabbah@gizasystems.com>
2024-12-15 21:27:05 +0200

    [user-management] track the user is new and Role

--- user-service/src/main/java/com/app/user/management/entity/BusinessUser.java
+++ user-service/src/main/java/com/app/user/management/entity/BusinessUser.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Persistable;
+
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Table(name = "business_user")
@@ -12,11 +16,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class BusinessUser {
+public class BusinessUser implements Persistable<Long> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @Column(name = "business_user_id")
+    private UUID businessUserId;
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
@@ -25,4 +31,19 @@ public class BusinessUser {
     private String email;
     @Column(name = "address")
     private String address;
+    @OneToMany(mappedBy = "businessUser", fetch = FetchType.LAZY)
+    private List<Role> roles;
+    @Transient
+    private boolean isNew = true;
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
+
+    @PostLoad
+    @PrePersist
+    public void trackAsNotNew() {
+        this.isNew = false;
+    }
 }
---
+++ user-service/src/main/java/com/app/user/management/entity/Role.java
@@ -0,0 +1,29 @@
+package com.app.user.management.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "role")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "code")
+    private String code;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "business_user_id")
+    private BusinessUser businessUser;
+}
