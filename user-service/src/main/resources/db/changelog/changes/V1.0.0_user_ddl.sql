7e81dd9fbf7fc536c374669dea5cfc3bb13e1e1c
Muhammed Sabbah <30741844+muhammedSabbah@users.noreply.github.com>
2024-12-15 21:33:42 +0200

    Feature/init user (#9)
    * Initialize the base of User
    
    * [user-management] track the user is new and Role
    
    ---------
    
    Co-authored-by: mohamed.sabbah <mohamed.sabbah@gizasystems.com>

--- user-service/src/main/resources/db/changelog/changes/V1.0.0_user_ddl.sql
+++ user-service/src/main/resources/db/changelog/changes/V1.0.0_user_ddl.sql
@@ -1,9 +1,21 @@
 CREATE TABLE business_user
 (
-    id          serial NOT NULL AUTO_INCREMENT,
-    first_name  VARCHAR (255),
-    last_name   VARCHAR (255),
-    email       VARCHAR (255),
-    address     VARCHAR (255),
+    id                  BIGINT NOT NULL AUTO_INCREMENT,
+    business_user_id    BINARY (16) NOT NULL,
+    first_name          VARCHAR (255),
+    last_name           VARCHAR (255),
+    email               VARCHAR (255),
+    address             VARCHAR (255),
     CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+    id                  BIGINT NOT NULL AUTO_INCREMENT,
+    name                VARCHAR (100),
+    description         VARCHAR (255),
+    code                VARCHAR (100),
+    business_user_id    BIGINT,
+    CONSTRAINT pk_role PRIMARY KEY (id),
+    CONSTRAINT fk_role_business_user FOREIGN KEY (business_user_id) REFERENCES business_user (id)
 );
