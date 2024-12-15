c2d6c514b374d7ed0a838a80bb6c01c9fcf99b8f
mohamed.sabbah <mohamed.sabbah@gizasystems.com>
2024-12-15 17:20:27 +0200

    Initialize the base of User

---
+++ user-service/src/main/java/com/app/user/management/entity/BusinessUser.java
@@ -0,0 +1,28 @@
+package com.app.user.management.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "business_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BusinessUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "address")
+    private String address;
+}
